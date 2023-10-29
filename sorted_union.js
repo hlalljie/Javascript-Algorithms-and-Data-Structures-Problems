a4aea2117117e0093d0eb2f64ee77845731c0562
hlalljie <hlalljie@gmail.com>
2023-10-28 22:12:31 -0700

    Added sorted_union.js

---
+++ sorted_union.js
@@ -0,0 +1,27 @@
+/* Sorted Union */
+/*
+Write a function that takes two or more arrays and returns a new array of unique values in the order of the original provided arrays.
+
+In other words, all values present from all arrays should be included in their original order, but with no duplicates in the final array.
+
+The unique numbers should be sorted by their original order, but the final array should not be sorted in numerical order.
+
+Check the assertion tests for examples.
+*/
+
+function uniteUnique(arr) {
+  
+    let sorted_union_arr = [];
+    let values = new Map();
+    for (let i = 0; i < arguments.length; i++){
+      for (let j = 0; j < arguments[i].length; j++){
+        if (!(values.has(arguments[i][j]))){
+          sorted_union_arr.push(arguments[i][j]);
+          values.set(arguments[i][j], true);
+        }
+      }
+    }
+    return sorted_union_arr;
+  }
+  
+  console.log(uniteUnique([1, 3, 2], [5, 2, 1, 4], [2, 1]));
